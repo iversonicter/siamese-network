b05b6016f97b32b055e0522a2a120aecb3751c10
yongjie wang <yongjie.wang@ntu.edu.sg>
2019-06-02 20:00:00 +0800

    create_train.py

--- util/create_train.py
+++ util/create_train.py
@@ -15,12 +15,32 @@ def create_pair(items, num):
     label_list = []
 
     for i in range(num):
-        tmp
+        # generate positive pair
+        tmp = []
+        index = random.randint(len(items))
+        anchor = random.randint(len(items[index]))
+        positive = random.randint(len(items[index]))
+        while anchor == positive:
+            positive = random.randint(len(items[index]))
+        tmp.append(items[index][anchor])
+        tmp.append(items[index][positive])
+        image_list.append(tmp)
+        label_list.append(1)
 
+        # generate negative pair
+        for j in range(3):
+            tmp = []
+            index1 = random.randint(len(items))
+            while index == index1:
+                index1 = random.randint(len(items))
+            
+            tmp.append(items[index][anchor])
+            tmp.append(items[index1][negative])
+            image_list.append(tmp)
+            label_list.append(0)
 
 
     return image_list, label_list
-
 
 
 
