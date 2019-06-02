6f61e8fe68adbf862cb4641d00edaaf9d106f3af
yongjie wang <yongjie.wang@ntu.edu.sg>
2019-06-02 19:42:11 +0800

    create training samples

---
+++ util/create_train.py
@@ -0,0 +1,26 @@
+# Author: Wang Yongjie
+# Email:  yongjie.wang@ntu.edu.sg
+# Description: create training examples
+
+import os
+import random
+
+def create_pair(items, num):
+    """
+    items:  image subdirectory lists
+    num:    number of image pairs
+    """
+
+    image_list = []
+    label_list = []
+
+    for i in range(num):
+        tmp
+
+
+
+    return image_list, label_list
+
+
+
+
