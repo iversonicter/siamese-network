75d49dcea7e9a08b654b627de87b3dd728cd13e6
yongjie wang <yongjie.wang@ntu.edu.sg>
2019-06-04 10:49:55 +0800

    loader.py

---
+++ preprocess/loader.py
@@ -0,0 +1,30 @@
+# Author: Wang Yongjie
+# Email : yongjie.wang@ntu.edu.sg
+
+import os
+import torch
+import torch.utils.data as Data
+from PIL import Image
+from torchvision import transform
+
+class Face(Data.Dataset):
+
+    def __init__(self, images, labels, transform = None):
+        self.images = images
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.images)
+
+    def ___getitem__(self, index):
+        image, label = self.images[index], self.labels[index]
+        img1 = Image.open(image[0]).convert("RGB")
+        img2 = Image.open(image[1]).convert("RGB")
+
+        if self.transform is not None:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+
+        return img1, img2, label
+
